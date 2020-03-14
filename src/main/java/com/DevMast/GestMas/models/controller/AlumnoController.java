37304cd040b74d425bfe1eea21206a8d2d6ce1f9
Oscarjfdp <oscar1josue@hotmail.com>
2020-03-14 16:57:35 -0600

    Controladores Alumnos y Alumno Retirado

--- src/main/java/com/DevMast/GestMas/models/controller/AlumnoController.java
+++ src/main/java/com/DevMast/GestMas/models/controller/AlumnoController.java
@@ -1,5 +1,47 @@
 package com.DevMast.GestMas.models.controller;
 
+import java.util.List; 
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.DevMast.GestMas.models.entities.Alumnos;
+import com.DevMast.GestMas.models.services.AlumnoService;
+
+@RestController
+@RequestMapping("alumnos")
 public class AlumnoController {
+	
+	@Autowired
+	private AlumnoService alumnoService;
+	
+	
+	
+	@RequestMapping(value="/alumnos", method = RequestMethod.GET)
+	public List<Alumnos> obtenerAlumnos(){
+		List<Alumnos> alumnos = alumnoService.findAll();
+		return alumnos;
+	}
+	
+	@RequestMapping(value="/alumnos/{id}", method = RequestMethod.GET)
+	public Alumnos obtenerById(@PathVariable("id") Long id) {
+		Optional<Alumnos> alumnosById = alumnoService.findById(id);
+		return alumnosById.get();
+	}
+	
+	@RequestMapping(value="/alumnos", method = RequestMethod.POST)
+	public Alumnos guardar(@RequestBody Alumnos alumnos) {
+		Alumnos matriculado = alumnoService.save(alumnos);
+		return matriculado;
+	}
+	
+	@RequestMapping(value="/alumnos/{id}", method = RequestMethod.GET)
+	public void delete(@PathVariable("id")Long id) {
+		alumnoService.delete(id);
+	}
 
 }
