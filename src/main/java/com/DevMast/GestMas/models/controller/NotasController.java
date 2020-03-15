36bf4778abebf247d714c168ea576fafd34274d9
jennyaguilar57 <51379879+jennyaguilar57@users.noreply.github.com>
2020-03-14 19:08:06 -0600

    detalles corregidos

--- src/main/java/com/DevMast/GestMas/models/controller/NotasController.java
+++ src/main/java/com/DevMast/GestMas/models/controller/NotasController.java
@@ -1,5 +1,48 @@
 package com.DevMast.GestMas.models.controller;
 
-public class NotasController {
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.DevMast.GestMas.models.entities.Notas;
+import com.DevMast.GestMas.models.services.NotasService;
 
+
+
+@RestController
+@RequestMapping ("Notas")
+
+public class NotasController {
+	
+	@Autowired
+	private NotasService notasService;
+	
+	@RequestMapping(value="/notas", method = RequestMethod.GET)
+	public List<Notas> obtenerNotas(){
+		List<Notas> notas = notasService.findAll();
+		return notas;
+	}
+	
+	@RequestMapping (value="/notas/{id}", method =RequestMethod.GET)
+	public Optional<Notas> obtenerById(@PathVariable("id")Long id) {
+		Optional<Notas> notasById = notasService.findById(id);
+		return notasById;
+	}
+	
+	@RequestMapping(value="/notas", method = RequestMethod.POST)
+	public Notas guardar(@RequestBody Notas notas) {
+		Notas nota = notasService.save(notas);
+		return nota;
+	}
+	
+	@RequestMapping (value="/notas/{id}", method = RequestMethod.GET)
+	public void delete (@PathVariable("id") Long id) {
+		notasService.delete(id);
+	}
+	
 }
+
--- src/main/java/com/DevMast/GestMas/models/controller/UsuariosController.java
+++ src/main/java/com/DevMast/GestMas/models/controller/UsuariosController.java
@@ -1,5 +1,45 @@
 package com.DevMast.GestMas.models.controller;
 
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.DevMast.GestMas.models.entities.Usuarios;
+import com.DevMast.GestMas.models.services.UsuariosService;
+
+@RestController
+@RequestMapping ("Usuarios")
+
 public class UsuariosController {
+	
+	@Autowired
+	private UsuariosService usuariosService;
+	
+	@RequestMapping(value="/usuarios", method = RequestMethod.GET)
+	public List<Usuarios> obtenerUsuarios(){
+		List<Usuarios> usuarios = usuariosService.findAll();
+		return usuarios;
+	}
+	
+	@RequestMapping (value="/usuarios/{id}", method =RequestMethod.GET)
+	public Optional<Usuarios> obtenerById(@PathVariable("id")Long id) {
+		Optional<Usuarios> usuariosById = usuariosService.findById(id);
+		return usuariosById;
+	}
+	
+	@RequestMapping(value="/usuarios", method = RequestMethod.POST)
+	public Usuarios guardar(@RequestBody Usuarios usuarios) {
+		Usuarios usuario = usuariosService.save(usuarios);
+		return usuario;
+	}
+	
+	@RequestMapping (value="/usuarios/{id}", method = RequestMethod.GET)
+	public void delete (@PathVariable("id") Long id) {
+		usuariosService.delete(id);
+	}
 
 }
